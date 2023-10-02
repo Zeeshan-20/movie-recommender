d2984118ae9c13c9d1e0203546ce24570c6ea13d
Zeeshan Khan <zeeshankhn00786@gmail.com>
2023-10-02 23:56:26 +0530

    Commit 3

--- movie-rec-sys/app.py
+++ movie-rec-sys/app.py
@@ -32,10 +32,10 @@ movies= pd.DataFrame(movies_dict)
 similarity=pickle.load(open('similarity.pkl','rb'))
 
 
-st.title('Move Recommender System')
+st.title('Movie Recommender System')
 
 selected_movie_name = st.selectbox(
-    'how are you',
+    'Look Alike Movies',
     movies['title'].values
 )
 
